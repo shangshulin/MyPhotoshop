32676899f218053e81d9fc4a3e4652517cf80842
LXDXB <2356641847@qq.com>
2025-03-18 15:25:39 +0800

    CImageProc.h
    16、24、32位封装

--- MyPhotoshop/CImageProc.h
+++ MyPhotoshop/CImageProc.h
@@ -1,22 +1,30 @@
 #pragma once
+
 class CImageProc
 {
 public:
-	CImageProc();
-	~CImageProc();
-	HGLOBAL m_hDib;
-	BYTE* pDib;
-	BITMAPFILEHEADER* pBFH;
-	BITMAPINFOHEADER* pBIH;
-	RGBQUAD* pQUAD;
-	BYTE* pBits;
-	int nWidth;
-	int nHeight;
-	int nNumColors;
-public:
-	void OpenFile();
-	void LoadBmp(CString stFileName);
-	void ShowBMP(CDC* pDC);
-	void GetColor(CClientDC* pDC, int x, int y);
-};
+    CImageProc();
+    ~CImageProc();
 
+private:
+    HGLOBAL m_hDib;
+    BYTE* pDib;
+    BITMAPFILEHEADER* pBFH;
+    BITMAPINFOHEADER* pBIH;
+    RGBQUAD* pQUAD;
+    BYTE* pBits;
+    int nWidth;
+    int nHeight;
+    int nNumColors;
+
+public:
+    void OpenFile();
+    void LoadBmp(CString stFileName);
+    void ShowBMP(CDC* pDC);
+    void GetColor(CClientDC* pDC, int x, int y);
+
+private:
+    void GetColor16(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+    void GetColor24(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+    void GetColor32(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+};
