230f2c0b00dddf3739937996abfeed67276ff41e
LXDXB <2356641847@qq.com>
2025-03-20 22:49:49 +0800

    已完成16、24、32位

--- MyPhotoshop/CImageProc.h
+++ MyPhotoshop/CImageProc.h
@@ -1,30 +1,30 @@
 #pragma once
-
 class CImageProc
 {
 public:
-    CImageProc();
-    ~CImageProc();
-
-private:
-    HGLOBAL m_hDib;
-    BYTE* pDib;
-    BITMAPFILEHEADER* pBFH;
-    BITMAPINFOHEADER* pBIH;
-    RGBQUAD* pQUAD;
-    BYTE* pBits;
-    int nWidth;
-    int nHeight;
-    int nNumColors;
-
+	CImageProc();
+	~CImageProc();
+	HGLOBAL m_hDib;
+	BYTE* pDib;
+	BITMAPFILEHEADER* pBFH;
+	BITMAPINFOHEADER* pBIH;
+	RGBQUAD* pQUAD;
+	BYTE* pBits;
+	int nWidth;
+	int nHeight;
+	int nNumColors;
+	int rowSize;
 public:
-    void OpenFile();
-    void LoadBmp(CString stFileName);
-    void ShowBMP(CDC* pDC);
-    void GetColor(CClientDC* pDC, int x, int y);
-
+	void OpenFile();
+	void LoadBmp(CString stFileName);
+	void ShowBMP(CDC* pDC);
+	void GetColor(CClientDC* pDC, int x, int y);
 private:
-    void GetColor16(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
-    void GetColor24(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
-    void GetColor32(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+	void GetColor1bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue, int x, int y, CDC* pDC);
+	void GetColor2bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue, int x);
+	void GetColor4bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue, int x);
+	void GetColor8bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue, int x);
+	void GetColor16bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+	void GetColor24bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
+	void GetColor32bit(BYTE* pixel, BYTE& red, BYTE& green, BYTE& blue);
 };
