e7a9c49e3c018e93777ae30ef0e8010b0888913a
尚舒林 <1367241289@qq.com>
2025-04-02 00:09:07 +0800

    新增类CHistogramDlg
    用于显示灰度直方图

---
+++ MyPhotoshop/CHistogramDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CHistogramDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MyPhotoshop.h"
+#include "afxdialogex.h"
+#include "CHistogramDlg.h"
+
+
+// CHistogramDlg 对话框
+
+IMPLEMENT_DYNAMIC(CHistogramDlg, CDialogEx)
+
+CHistogramDlg::CHistogramDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CHistogramDlg, pParent)
+{
+
+}
+
+CHistogramDlg::~CHistogramDlg()
+{
+}
+
+void CHistogramDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CHistogramDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CHistogramDlg 消息处理程序
--- MyPhotoshop/Resource.h
+++ MyPhotoshop/Resource.h
@@ -2,18 +2,43 @@
 // 生成的 Microsoft Visual C++ 包含文件。
 // 由 MyPhotoshop.rc 使用
 //
+<<<<<<< Updated upstream
 #define IDD_ABOUTBOX				100
 #define IDP_OLE_INIT_FAILED			100
 #define IDR_MAINFRAME				128
 #define IDR_MyPhotoshopTYPE				130
+=======
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MyPhotoshopTYPE             130
+#define IDD_CHistogramDlg               310
+#define ID_32771                        32771
+#define ID_PIXEL_INFO                   32772
+#define ID_VIEW_PIXELINFO               32773
+#define ID_32774                        32774
+#define ID_SHOW_                        32775
+#define ID_SHOW_HISTOGRAM               32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+>>>>>>> Stashed changes
 
 // 新对象的下一组默认值
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+<<<<<<< Updated upstream
 #define _APS_NEXT_RESOURCE_VALUE	310
 #define _APS_NEXT_CONTROL_VALUE		1000
 #define _APS_NEXT_SYMED_VALUE		310
 #define _APS_NEXT_COMMAND_VALUE		32771
+=======
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           311
+>>>>>>> Stashed changes
 #endif
 #endif
